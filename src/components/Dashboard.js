63c6006e8b246ad9d21e7b792f08907576ec8d24
Justin <justinjdbennett@gmail.com>
2023-01-19 23:44:05 -0700

    Get data from scheduler-api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import axios from 'axios';
 
 import Loading from './Loading';
 import Panel from './Panel';
+
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from 'helpers/selectors';
 
 const data = [
   {
     id: 1,
     label: 'Total Interviews',
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: 'Least Popular Time Slot',
-    value: '1pm',
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: 'Most Popular Day',
-    value: 'Wednesday',
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: 'Interviews Per Day',
-    value: '2.3',
+    getValue: getInterviewsPerDay,
   },
 ];
 
@@ -31,10 +39,26 @@ class Dashboard extends Component {
   state = {
     loading: true,
     focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
   };
 
   componentDidMount() {
     const focused = JSON.parse(localStorage.getItem('focused'));
+
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers'),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
 
     if (focused) {
       this.setState({ focused });
@@ -66,7 +90,7 @@ class Dashboard extends Component {
       <Panel
         key={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
         onSelect={(e) => this.selectPanel(panel.id)}
       />
     ));
